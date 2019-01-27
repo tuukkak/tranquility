0b8361c428ead3febe05601f92f76e81f52af789
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2019-01-27 21:44:58 +0200

    Sepll assets

---
+++ Assets/Spells/Spell.cs
@@ -0,0 +1,19 @@
+﻿public class Spell {
+    public int Id { get; private set; }
+    public int HeroId { get; private set; }
+    public string Name { get; private set; }
+    public float Range { get; private set; }
+    public float CastTime { get; private set; }
+    public float Speed { get; private set; }
+    public string KeyCode { get; private set; }
+
+    public Spell(int id, int heroId, string keyCode, string name, float range, float castTime = 0f, float speed = 0f) {
+        Id = id;
+        HeroId = heroId;
+        Name = name;
+        Range = range;
+        CastTime = castTime;
+        Speed = speed;
+        KeyCode = keyCode;
+    }
+}
---
+++ Assets/Spells/SpellAction.cs
@@ -0,0 +1,16 @@
+﻿public class SpellAction {
+    public enum ActionType { Start, Instant, Finish }
+    public ActionType Type { get; private set; }
+    public Player Caster { get; private set; }
+    public Player Target { get; private set; }
+    public Spell Spell { get; private set; }
+    public int Damage { get; private set; }
+
+    public SpellAction(ActionType type, Player caster, Player target, Spell spell, int damage = 0) {
+        Type = type;
+        Caster = caster;
+        Target = target;
+        Spell = spell;
+        Damage = damage;
+    }
+}
