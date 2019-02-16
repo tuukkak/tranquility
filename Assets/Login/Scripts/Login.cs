0bf2d243d91f8f81243ee70bd2eddd49160cdf3f
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2019-02-16 20:19:40 +0200

    Add login script

---
+++ Assets/Login/Scripts/Login.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    public InputField NameField;
+    public Button LoginButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoginButton.onClick.AddListener(OnLoginClick);
+    }
+
+    void OnLoginClick()
+    {
+
+        Network.Login(NameField.text);
+        //SceneManager.LoadScene("GameScene");
+    }
+}
