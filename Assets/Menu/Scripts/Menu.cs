73f2db5487063fbf911b1ccc70fb5700729e4150
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2019-02-22 19:19:28 +0200

    Add menu scene

---
+++ Assets/Menu/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Button JoinButton;
+
+    void Start()
+    {
+        JoinButton.onClick.AddListener(OnJoinClick);
+    }
+
+    void OnJoinClick()
+    {
+        Network.Join();
+        JoinButton.GetComponentsInChildren<Text>()[0].text = "Looking for game...";
+    }
+
+    void Update()
+    {
+        if (!string.IsNullOrEmpty(Game.LoadScene))
+        {
+            SceneManager.LoadScene(Game.LoadScene);
+            Game.LoadScene = "";
+        }
+    }
+}
